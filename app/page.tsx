10aed70e8bdd48eb7f8d298a13c4b8e821b614ca
Freddy Cunningham <freddy@fcunningham.co.uk>
2025-11-26 14:15:52 +0000

    Build basic form using react hook forms, zod and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,169 @@
-import Image from "next/image";
+"use client";
+
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+const formSchema = z.object({
+  nozzleId: z.string().min(1, "Select a nozzle"),
+  sprayVolumeLHa: z.coerce.number<number>()
+    .positive("Spray volume must be greater than 0"),
+  nozzleSpacingM: z.coerce.number<number>()
+    .positive("Nozzle spacing must be greater than 0")
+    .lt(10, "Nozzle spacing must be less than 10m"),
+  tankSizeL: z.coerce.number<number>()
+    .positive("Tank size must be greater than 0"),
+  speedKmH: z.coerce.number<number>()
+    .gte(3, `Min 3 km/h`)
+    .lte(12, `Max 12 km/h`),
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nozzleId: "syngenta-025-xc",
+      sprayVolumeLHa: 300,
+      nozzleSpacingM: 0.5,
+      tankSizeL: 400,
+      speedKmH: 5,
+    },
+    mode: "onChange",
+  })
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex justify-center mt-10">
+      <Card className="w-full max-w-lg">
+        <CardHeader>
+          <CardTitle>Spray Calculator</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form id="form" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller name="nozzleId" control={form.control} render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="input-nozzle">
+                    Nozzle
+                  </FieldLabel>
+                  <Select
+                    name={field.name}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger
+                      id="form-rhf-select-language"
+                      aria-invalid={fieldState.invalid}
+                    >
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem key={"syngenta-025-xc"} value="syngenta-025-xc">Syngenta 025 XC</SelectItem>
+                        <SelectItem key={"syngenta-04-xc"} value="syngenta-04-xc">Syngenta 04 XC</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )} />
+
+              <Controller name="sprayVolumeLHa" control={form.control} render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="input-sprayVolumeLHa">
+                    Spray volume (L/ha)
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="number"
+                    inputMode="numeric"
+                    id="sprayVolumeLHa"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Spray volume"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )} />
+
+              <Controller name="nozzleSpacingM" control={form.control} render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="input-nozzleSpacingM">
+                    Nozzle spacing (m)
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="number"
+                    inputMode="numeric"
+                    id="nozzleSpacingM"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Nozzle spacing"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )} />
+
+              <Controller name="tankSizeL" control={form.control} render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="input-tankSizeL">
+                    Tank size (L)
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="number"
+                    inputMode="numeric"
+                    id="tankSizeL"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Tank size"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )} />
+
+              <Controller name="speedKmH" control={form.control} render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="input-speedKmH">
+                    Speed (km/h) - {field.value}
+                  </FieldLabel>
+                  <Slider
+                    min={3}
+                    max={12}
+                    step={0.1}
+                    value={[field.value]}
+                    onValueChange={field.onChange}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )} />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="submit" form="form">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+      </Card>
     </div>
   );
 }
